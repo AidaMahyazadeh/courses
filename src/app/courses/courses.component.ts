06981f3d313ef65c07fae6c9301f56991e984803
AidaMahyazadeh <aidamahyazadeh83@gmail.com>
2023-03-18 09:53:14 +0100

    refactored the codes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -17,7 +17,7 @@ export class CoursesComponent {
       price: 129.00, ratings: 4.5, image:'assets/course-image-2.jpeg',
       description: 'In this course you will learn the fundamentals of Angular framework. This course is purely designed for beginners and teaches you all the basic concepts of Angular step by step'
     },
-    { id:103, name:'React for beginners', author: 'Steve Smith', duration: 18.5, type: 'Free', 
+    { id:103, name:'React course for beginners', author: 'Steve Smith', duration: 18.5, type: 'Free', 
       price: 0.00, ratings: 4.0, image:'assets/course-image-3.jpeg',
       description: 'In this course you will learn the fundamentals of React. This course is purely designed for beginners and teaches you all the basic concepts of React step by step'
     },
@@ -37,7 +37,7 @@ export class CoursesComponent {
       price: 0.00, ratings: 4.0, image:'assets/course-image-7.jpeg',
       description: 'In this course you will learn the fundamentals and advance concepts of React. This course is purely designed for beginners and teaches you all the concepts of React step by step'
     },
-    { id:108, name:'UX Design Course', author: 'Steve Smith', duration: 19.0, type: 'Premium', 
+    { id:108, name:'UX Design Course  for beginners', author: 'Steve Smith', duration: 19.0, type: 'Premium', 
       price: 115.50, ratings: 4.8, image:'assets/course-image-8.jpeg',
       description: 'In this course you will learn about reactive web development using HTML & CSS. This course will start teaching you from basics of HTML & CSS and as you progress, you will learn all the advance concepts.'
     },
